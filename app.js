deb4c02696ff2a51cadf6dc0c1acc1326c535b55
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-18 14:31:02 -0700

    Separate readFile and writeFile into separate functions wrapped in promises, pull toUpperCase from readfile to its own function

--- app.js
+++ app.js
@@ -2,13 +2,53 @@
 
 const fs = require('fs');
 
+/**
+ * @module alterFile
+ * @param {string} file - Name of the file to read from
+ * @desc Handles promise chain for read and write file
+ */
 const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
+  readFile(file)
+    .then(data => {
+      writeFile(file, caps(data));
+    });
+};
+
+/**
+ * @module readFile
+ * @param {string} file - Name of the file to read from
+ * @desc Handles reading the file and turning it's data to string
+ */
+let readFile = (file) => {
+  return new Promise((resolve) => {
+    fs.readFile( file, (err, data) => {
       if(err) { throw err; }
-      console.log(`${file} saved`);
+      resolve(data.toString());
+    });
+  });
+};
+
+/**
+ * @module caps
+ * @param {string} file - Name of the file to read from
+ * @param {string} text - The text from the file
+ * @desc Handles changing the text to all uppercase
+ */
+let caps = (data) => {
+  return data.toUpperCase();
+};
+
+/**
+ * @module writeFile
+ * @param {string} file - Name of the file to read from
+ * @param {string} text - The text from the file
+ * @desc Writes the text to the file
+ */
+let writeFile = (file, text) => {
+  return new Promise(() => {
+    fs.writeFile( file, Buffer.from(text), (err) => {
+      if(err) { throw err; }
+      console.log(`${file} saved!`);
     });
   });
 };
