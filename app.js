7904a38f8579d4b42c1de76a69337413a73bae40
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-18 15:23:50 -0700

    Put readFile, writeFile, and caps from readWriteCaps.js back into app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const utility = require('./readWriteCaps.js');
+const fs = require('fs');
 
 /**
  * @module alterFile
@@ -8,10 +8,49 @@ const utility = require('./readWriteCaps.js');
  * @desc Handles promise chain for read and write file
  */
 const alterFile = (file) => {
-  utility.readFile(file)
+  readFile(file)
     .then(data => {
-      utility.writeFile(file, utility.caps(data));
+      writeFile(file, caps(data));
     });
+};
+
+/**
+ * @module readFile
+ * @param {string} file - Name of the file to read from
+ * @desc Handles reading the file and turning it's data to string
+ */
+let readFile = (file) => {
+  return new Promise((resolve) => {
+    fs.readFile( file, (err, data) => {
+      if(err) { throw err; }
+      resolve(data.toString());
+    });
+  });
+};
+
+/**
+ * @module caps
+ * @param {string} file - Name of the file to read from
+ * @param {string} text - The text from the file
+ * @desc Handles changing the text to all uppercase
+ */
+let caps = (data) => {
+  return data.toUpperCase();
+};
+
+/**
+ * @module writeFile
+ * @param {string} file - Name of the file to read from
+ * @param {string} text - The text from the file
+ * @desc Writes the text to the file
+ */
+let writeFile = (file, text) => {
+  return new Promise(() => {
+    fs.writeFile( file, Buffer.from(text), (err) => {
+      if(err) { throw err; }
+      console.log(`${file} saved!`);
+    });
+  });
 };
 
 let file = process.argv.slice(2).shift();
